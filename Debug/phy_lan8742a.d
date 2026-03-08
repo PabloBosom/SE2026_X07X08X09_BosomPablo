90178006e23a148e4b23a171d2edf14fe3b0b370
PABLO <p.bosom@alumnos.upm.es>
2026-03-08 13:32:16 +0100

    Practica 1

---
+++ Debug/phy_lan8742a.d
@@ -0,0 +1,8 @@
+.\debug\phy_lan8742a.o: C:/Users/PcVIP/AppData/Local/Arm/Packs/ARM/CMSIS-Driver/2.6.1/ETH/PHY_LAN8742A.c
+.\debug\phy_lan8742a.o: C:/Users/PcVIP/AppData/Local/Arm/Packs/ARM/CMSIS-Driver/2.6.1/ETH/PHY_LAN8742A.h
+.\debug\phy_lan8742a.o: C:\Users\PcVIP\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Driver\Include\Driver_ETH_PHY.h
+.\debug\phy_lan8742a.o: C:\Users\PcVIP\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Driver\Include\Driver_ETH.h
+.\debug\phy_lan8742a.o: C:\Users\PcVIP\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Driver\Include\Driver_Common.h
+.\debug\phy_lan8742a.o: C:\Users\PcVIP\AppData\Local\Keil_v5\ARM\ARMCC_506\Bin\..\include\stddef.h
+.\debug\phy_lan8742a.o: C:\Users\PcVIP\AppData\Local\Keil_v5\ARM\ARMCC_506\Bin\..\include\stdint.h
+.\debug\phy_lan8742a.o: C:\Users\PcVIP\AppData\Local\Keil_v5\ARM\ARMCC_506\Bin\..\include\stdbool.h
---
+++ Debug/web.d
@@ -0,0 +1,3 @@
+.\debug\web.o: Web\Web.c
+.\debug\web.o: C:\Users\PcVIP\AppData\Local\Keil_v5\ARM\ARMCC_506\Bin\..\include\stddef.h
+.\debug\web.o: C:\Users\PcVIP\AppData\Local\Keil_v5\ARM\ARMCC_506\Bin\..\include\stdint.h
